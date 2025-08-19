2c49aa20098f3be54aec692edeb288989d720dea
tylerzimmermann <Tyler.zimmermann1@gmail.com>
2025-08-18 23:52:54 -0500

    Create index.js

---
+++ src/animations/index.js
@@ -0,0 +1,12 @@
+// src/animations/index.js
+
+import RocketDog from "./RocketDog.json";
+import clap from "./clap.json";
+import violin from "./violin.json";
+
+
+export const lottieAnimations = {
+  RocketDog,
+  clap,
+  violin,
+};
