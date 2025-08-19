8faac2814296295b44d38a5607ab588c2f90a6ed
tylerzimmermann <Tyler.zimmermann1@gmail.com>
2025-08-19 00:00:34 -0500

    Update students.js

--- src/data/students.js
+++ src/data/students.js
@@ -1,8 +1,9 @@
 export const students = [
   {
     name: "Violet",
-    color: "#A8D0E6", // soft blue
+    color: "#A8D0E6",
     currentSong: "Minuet 2",
+    avatar: "/avatars/violet.png", // put this file in your public/avatars folder
     media: {
       mp3: [],
       photos: [],
@@ -11,8 +12,9 @@ export const students = [
   },
   {
     name: "Theo",
-    color: "#FBC490", // soft orange
+    color: "#FBC490",
     currentSong: "Long, Long Ago",
+    avatar: "/avatars/theo.png", // put this file in your public/avatars folder
     media: {
       mp3: [],
       photos: [],
@@ -21,7 +23,8 @@ export const students = [
   },
 ];
 
-// Utility function to get songs per student
+// Utility function to get media for a specific student
 export function getStudentSongs(studentName) {
-  return students.find((s) => s.name === studentName)?.media || {};
+  const student = students.find((s) => s.name === studentName);
+  return student ? student.media : { mp3: [], photos: [], videos: [] };
 }
