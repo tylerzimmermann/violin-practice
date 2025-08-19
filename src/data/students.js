f281eab5f7f4415e6031acd8a5f34ba1ead7b94f
tylerzimmermann <Tyler.zimmermann1@gmail.com>
2025-08-18 22:39:03 -0500

    Create students.js

---
+++ src/data/students.js
@@ -0,0 +1,16 @@
+// src/data/students.js
+
+export const students = [
+  {
+    id: "violet",
+    name: "Violet",
+    color: "#89CFF0", // soft blue
+    avatar: "/avatars/violet.png", // put an image in /public/avatars
+  },
+  {
+    id: "theo",
+    name: "Theo",
+    color: "#FFB347", // soft orange
+    avatar: "/avatars/theo.png",
+  },
+];
